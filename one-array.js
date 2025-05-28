5c26430ac7da859188981dfbd9f9cfcd1590f1b8
Anty2767 <116466187+Anty2767@users.noreply.github.com>
2025-05-28 13:32:19 +0200

    file iniziale

---
+++ one-array.js
@@ -0,0 +1,47 @@
+const message = 'Hello world' // Try edit me
+
+
+const messages = [];
+
+messages.push("ciao");
+
+if( "ciao" === "Ciao") {
+
+}
+
+const cf = [
+    "RSSMRA85M01H501Z",
+    "BNCLRD90A22F205X",
+    "VRDLNZ75B12D612Y",
+    "FRNGPP80C11L219W",
+    "MNLGLD70D15H501Z",
+    "BRRMLT85E01F205X",
+    "CNRMRA88F22G219Y",
+    "PZZLNN90G18D612W",
+    "TSTMRA78H15L219Z",
+    "VLDLND85J21F205X",
+    "SGRLNZ70K01G612Y",
+    "RCCMRA65L11H219W",
+    "BLZNDR80M22F219Z",
+    "MRTGPP75N12D205X",
+    "DNLFRA90P11G501Y",
+    "CRLVND88Q21H219W",
+    "PPLMRA70R15F612Z",
+    "LZZMRA85S01D205X",
+    "FRNGPP75T22G501Y",
+    "MNLGPP90U18H219W",
+    "BRTGPP80V11F219Z",
+    "CNRLNZ85W21D205X",
+    "VRDLND75X15G501Y",
+    "SGRMRA90Y01H219W",
+    "RSSMRN80Z12F612Z"
+];
+
+// Ordinare i codici fiscali in ordine alfabetico
+// Suddividere l'array padre in n (iniziale)
+
+// Update header text
+document.querySelector('#header').innerHTML = message
+
+// Log to console
+console.log(message)
